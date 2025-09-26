18ed2e96e7ea80913b00b6981eb344fd885e03a9
Hanieh424 <hsadegh4@my.centennialcollege.ca>
2025-09-26 17:27:37 -0400

    Update vite.config.js

--- comp229-401-portfolio/vite.config.js
+++ comp229-401-portfolio/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['comp229-sec401-portfolio-assignment1.onrender.com'],
-    host: true,
-    port: 3000
+    allowedHosts:[
+      'comp229-sec401-portfolio-assignment1.onrender.com'
+    ]
   }
 })
